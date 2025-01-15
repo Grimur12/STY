920fa10b0f02aedc525a6588698ddabdd6d03f68
Grimur <dgrimur@gmail.com>
2025-01-15 14:24:54 +0000

    g

--- STY A1/assignment01-templates/p2/main.c
+++ STY A1/assignment01-templates/p2/main.c
@@ -7,6 +7,10 @@ int main()
 
     test_equals_int(convertDecimalDigit('0'), 0);
     test_equals_int(parseInt("010"), 8);
+    test_equals_int(parseInt("01"), 1);
+    test_equals_int(parseInt("0"), 0);
+    test_equals_int(parseInt("563"), 563);
+    test_equals_int(parseInt("0563"), 371);
     test_equals_int(parseInt("10"), 10);
     test_equals_int(parseInt("?"), -1);
 
